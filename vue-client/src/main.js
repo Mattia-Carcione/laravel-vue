92e9c3fec556db1e7e7e670561609896082dfc70
Mattia <mattia@MBP-di-Mattia.station>
2023-11-17 12:28:41 +0100

    add vue

---
+++ vue-client/src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import '@fortawesome/fontawesome-free'
+
+const app = createApp(App)
+
+app.use(router, VueAxios, axios)
+
+app.mount('#app')
---
+++ vue-client/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
