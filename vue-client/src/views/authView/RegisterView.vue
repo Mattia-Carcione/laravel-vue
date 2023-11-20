c035fe4761aae10e64a746536096b07ca7148f7f
Mattia <mattia@MBP-di-Mattia.station>
2023-11-20 18:20:19 +0100

    remove console log

--- vue-client/src/views/authView/RegisterView.vue
+++ vue-client/src/views/authView/RegisterView.vue
@@ -25,19 +25,15 @@ export default {
     },
     methods: {
         async registerUser() {
-            console.log('Registering user...');
             this.showPassword = false;
             this.showPasswordConfirm = false;
             await useAuth().registerUser(this.form);
-            console.log('Registration completed.');
             this.form.error = this.state.errors;
             this.redirect(this.state.authenticated);
         },
         redirect(boolean: boolean) {
             if (boolean) {
-                console.log('Redirecting to home...');
                 this.$router.push({ name: 'profile' })
-                console.log('Redirected to home.');
             }
         },
         togglePasswordVisibility() {
