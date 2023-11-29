d935d5d8699ff695ea1a74fb64b54d92612a40c8
Mattia <mattia@MBP-di-Mattia.station>
2023-11-29 20:52:44 +0100

    add bg white

--- vue-client/src/components/ButtonCategory.vue
+++ vue-client/src/components/ButtonCategory.vue
@@ -7,12 +7,11 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col justify-center">
-        <p
-            class="inline-flex items-center justify-center rounded-full py-4 px-10 text-base font-medium text-black transition duration-300 ease-in-out hover:bg-opacity-90">
+    <div class="flex flex-col justify-center bg-white">
+        <p class="inline-flex items-center justify-center rounded-full py-4 px-10 text-base font-medium text-black transition duration-300 ease-in-out hover:bg-opacity-90">
             See All Category:
         </p>
-        <div class="flex px-1 lg:px-40 bg-white items-center justify-center pt-4 flex-wrap">
+        <div class="flex px-1 lg:px-40 items-center justify-center pt-4 flex-wrap">
             <RouterLink :to="{ name: 'category', params: { name: category.slug } }" v-for="category in categories"
                 :key="category.id">
                 <button type="button"
