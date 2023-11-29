6e5d745a1bbc203c85bffd6c0dceeeb26a4d85b9
Mattia <mattia@MBP-di-Mattia.station>
2023-11-29 18:16:00 +0100

    modifiche front end

--- vue-client/src/views/404View.vue
+++ vue-client/src/views/404View.vue
@@ -2,15 +2,25 @@
 </script>
 
 <template>
-    <main class="min-h-screen">
-    </main>
-</template>
+    <div class="flex items-center justify-center w-full min-h-screen bg-gray-100">
+        <div class="flex flex-col text-gray-700 lg:flex-row lg:space-x-16 lg:space-x-reverse">
+            <div class="order-1 max-w-md px-2 text-sm md:text-base lg:px-0">
+                <header class="mb-6">
+                    <h2 class="text-4xl font-bold leading-none text-gray-400 select-none lg:text-6xl">404.</h2>
+                    <h3 class="text-xl font-light leading-normal lg:text-3xl md:text-3xl">Sorry, we couldn't find this page.
+                    </h3>
+                </header>
 
-<style scoped>
-main {
-    background-position: top center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-image: url('https://cdn2.hubspot.net/hubfs/242200/shutterstock_774749455.jpg');
-}
-</style>
+                <p class="max-w-sm mb-5 leading-5 md:leading-7">
+                    Don't worry, sometimes even we make mistakes.
+                    You can find plenty of other things on our homepage.
+                </p>
+
+                <a href="/"
+                    class="inline px-4 py-2 text-sm font-medium leading-5 text-white uppercase transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue active:bg-blue-600 hover:bg-blue-700">
+                    Back to Homepage
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
