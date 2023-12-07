a67ba5e6a9ccc27047d43dd68897168bd0fd604b
Mattia <mattia@MBP-di-Mattia.station>
2023-12-07 12:18:46 +0100

    refactoring register View

--- vue-client/src/router/index.js
+++ vue-client/src/router/index.js
@@ -12,140 +12,13 @@ import AppereanceView from '../views/authView/AppereanceView.vue';
 
 const auth = useAuth()
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: () => import('../views/HomeView.vue')
-//     },
-//     {
-//       path: '/about',
-//       name: 'about',
-//       // route level code-splitting
-//       // this generates a separate chunk (About.[hash].js) for this route
-//       // which is lazy-loaded when the route is visited.
-//       component: () => import('../views/AboutView.vue')
-//     },
-//     {
-//       path: '/login',
-//       name: 'login',
-//       component: () => import('../components/authForm/LoginView.vue')
-//     },
-//     {
-//       path: '/register',
-//       name: 'register',
-//       component: () => import('../components/authForm/RegisterView.vue')
-//     },
-//     {
-//       path: '/profile',
-//       name: 'profile',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/profile/dashboard',
-//       name: 'profile/dashboard',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/profile/dashboard/create',
-//       name: 'profile/table',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/profile/revisor',
-//       name: 'revisor',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           if (auth.getUser.value.is_revisor) {
-//             next()
-//           } else {
-//             next({ name: 'home' })
-//           }
-//         } 
-//       }
-//     },
-//     {
-//       path: '/profile/edit',
-//       name: 'profile/edit',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/profile/privacy',
-//       name: 'profile/privacy',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/profile/appereance',
-//       name: 'profile/appereance',
-//       component: () => import('../templateUser/Profile.vue'),
-//       beforeEnter: (to, from, next) => {
-//         if (auth.getAuthenticated.value) {
-//           next()
-//         } else {
-//           next('/login')
-//         }
-//       }
-//     },
-//     {
-//       path: '/announcements',
-//       name: 'announcements',
-//       component: () => import('../views/announceView/Index.vue'),
-//     },
-//     {
-//       path: '/announcements/:name',
-//       name: 'announcement/show',
-//       component: () => import('../views/announceView/Show.vue'),
-//     },
-//     {
-//       path: '/category/:name',
-//       name: 'category',
-//       component: () => import('../views/CategoryView.vue'),
-//     },
-//     {
-//       path: '/:pathMatch(.*)*',
-//       name: 'not-found',
-//       component: () => import('../views/404View.vue'),
-//     }
-//   ]
-// })
+const isAuthenticated = (to, from, next) => {
+  if (auth.getAuthenticated.value) {
+    next('/');
+  } else {
+    next();
+  }
+}
 
 const routes = [
   {
@@ -156,17 +29,22 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import('../views/AboutView.vue')
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/authViews/LoginView.vue')
+    component: () => import('../views/authViews/LoginView.vue'),
+    beforeEnter: (isAuthenticated)
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import('../components/authForm/RegisterView.vue')
+    component: () => import('../views/authViews/RegisterView.vue'),
+    beforeEnter: (isAuthenticated)
   },
   {
     path: '/profile',
--- vue-client/src/views/authViews/RegisterView.vue
+++ vue-client/src/views/authViews/RegisterView.vue
@@ -1,52 +1,3 @@
-<script lang="ts">
-import useAuth from '../../auth/useAuth'
-import { reactive } from 'vue';
-import { RouterLink } from 'vue-router';
-export default {
-    data() {
-        const state = reactive({
-            authenticated: useAuth().getAuthenticated,
-            user: useAuth().getUser,
-            errors: useAuth().getErrors,
-        });
-        return {
-            state,
-            form: {
-                name: '',
-                email: '',
-                password: '',
-                password_confirmation: '',
-                error: '',
-            },
-            showPassword: false,
-            showPasswordConfirm: false,
-            auth: useAuth()
-        }
-    },
-    methods: {
-        async registerUser() {
-            this.showPassword = false;
-            this.showPasswordConfirm = false;
-            await useAuth().registerUser(this.form);
-            this.form.error = this.state.errors;
-            this.redirect(this.state.authenticated);
-        },
-        redirect(boolean: boolean) {
-            if (boolean) {
-                this.$router.push({ name: 'profile' })
-            }
-        },
-        togglePasswordVisibility() {
-            this.showPassword = !this.showPassword;
-        },
-        togglePasswordConfirmVisibility() {
-            this.showPasswordConfirm = !this.showPasswordConfirm;
-        }
-    }
-}
-
-</script>
-
 <template>
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">Create an Account
@@ -54,44 +5,44 @@ export default {
     </div>
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <!-- Register form -->
-        <form class="space-y-6 mx-2 lg:mx-0" action="#" method="POST" @submit.prevent="registerUser">
+        <form class="space-y-6 mx-2 lg:mx-0" action="#" method="POST" @submit.prevent="register(form)">
 
             <!-- Validation name error -->
-            <div v-if="form.error.name"
-                class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <span v-if="state.errors.name" class="font-bold block sm:inline">{{ state.errors.name[0] }}</span>
+            <div v-if="error.name" class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert">
+                <span class="font-bold block sm:inline">{{ error.name[0] }}</span>
             </div>
 
             <!-- Name input -->
             <div>
                 <label for="name" class="block text-sm font-medium leading-6">Your Name</label>
                 <div class="mt-2">
-                    <input :class="{ 'border-red-700': form.error.name, 'border-2': form.error.name }" v-model="form.name"
-                        id="name" name="name" type="text" placeholder="First Name Last Name" required
+                    <input :class="{ 'border-red-700': error.name, 'border-2': error.name }" v-model="form.name" id="name"
+                        name="name" type="text" placeholder="First Name Last Name" required
                         class="block w-full rounded-md border-0 px-3 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
 
             <!-- Validation email error -->
-            <div v-if="form.error.email"
-                class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <span v-if="state.errors.email" class="font-bold block sm:inline">{{ state.errors.email[0] }}</span>
+            <div v-if="error.email" class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert">
+                <span v-if="error.email" class="font-bold block sm:inline">{{ error.email[0] }}</span>
             </div>
 
             <!-- Email input -->
             <div>
                 <label for="email" class="block text-sm font-medium leading-6">Email</label>
                 <div class="mt-2">
-                    <input :class="{ 'border-red-700': form.error.email, 'border-2': form.error.email }"
-                        v-model="form.email" id="email" name="email" type="email" required placeholder="Enter your email"
+                    <input :class="{ 'border-red-700': error.email, 'border-2': error.email }" v-model="form.email"
+                        id="email" name="email" type="email" required placeholder="Enter your email"
                         class="block w-full px-3 rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
 
             <!-- Validation password error -->
-            <div v-if="form.error.password"
-                class="flex bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <span v-if="state.errors.password" class="font-bold block sm:inline">{{ state.errors.password[0] }}</span>
+            <div v-if="error.password" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert">
+                <span v-for="error in error.password" class="font-bold pr-1 block sm:inline">{{ error }}</span>
             </div>
 
             <!-- Password input -->
@@ -100,14 +51,14 @@ export default {
                     <label for="password" class="block text-sm font-medium leading-6">Choose Password</label>
                 </div>
                 <div class="mt-2 relative rounded-md shadow-sm">
-                    <input :class="{ 'border-red-700': form.error.password, 'border-2': form.error.password }"
-                        v-model="form.password" :type="showPassword ? 'text' : 'password'" id="password" name="password"
-                        required placeholder="Password"
+                    <input :class="{ 'border-red-700': error.password, 'border-2': error.password }" v-model="form.password"
+                        :type="showPassword ? 'text' : 'password'" id="password" name="password" required
+                        placeholder="Password"
                         class="form-input py-2 px-3 block w-full leading-5 rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600" />
                     <div class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
                         <!-- toggle password -->
-                        <button @click="togglePasswordVisibility" type="button"
-                            class="text-gray-500 focus:outline-none focus:text-gray-700">
+                        <button @click="toggleShowPassword" type="button"
+                            class="text-gray-500 focus:outline-none">
                             <span v-if="showPassword"><i class=" far fa-eye"></i></span>
                             <span v-else><i class=" far fa-eye-slash"></i></span>
                         </button>
@@ -122,15 +73,14 @@ export default {
                         Password</label>
                 </div>
                 <div class="mt-2 relative rounded-md shadow-sm">
-                    <input
-                        :class="{ 'border-red-700': form.error.password, 'border-2': form.error.password }"
+                    <input :class="{ 'border-red-700': error.password, 'border-2': error.password }"
                         v-model="form.password_confirmation" :type="showPasswordConfirm ? 'text' : 'password'"
                         id="password-confirm" name="password-confirm" required placeholder="Confirm Password"
                         class="form-input py-2 px-3 block w-full leading-5 rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600" />
                     <div class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
                         <!-- toggle password confirm -->
-                        <button @click="togglePasswordConfirmVisibility" type="button"
-                            class="text-gray-500 focus:outline-none focus:text-gray-700">
+                        <button @click="toggleShowPasswordConfirm" type="button"
+                            class="text-gray-500 focus:outline-none">
                             <span v-if="showPasswordConfirm"><i class=" far fa-eye"></i></span>
                             <span v-else><i class=" far fa-eye-slash"></i></span>
                         </button>
@@ -155,4 +105,48 @@ export default {
     </div>
 </template>
 
-<style scoped></style>
+<script>
+import useAuth from '../../auth/useAuth'
+import { RouterLink } from 'vue-router';
+export default {
+    data() {
+        return {
+            form: {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+            },
+            error: {
+                name: '',
+                email: '',
+                password: '',
+            },
+            showPassword: false,
+            showPasswordConfirm: false,
+        }
+    },
+    methods: {
+        async register(form) {
+            this.showPassword = false;
+            this.showPasswordConfirm = false;
+            const auth = useAuth();
+            await auth.register(form);
+            this.catchError(auth.getErrors.value);
+        },
+        catchError(error) {
+            if (error) {
+                this.error = error;
+            } else {
+                this.$router.push({ name: 'profile' });
+            }
+        },
+        toggleShowPassword() {
+            this.showPassword = !this.showPassword;
+        },
+        toggleShowPasswordConfirm() {
+            this.showPasswordConfirm = !this.showPasswordConfirm;
+        }
+    }
+}
+</script>
